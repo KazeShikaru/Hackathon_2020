3627016d41920d64f0e1c71a571fcf2c3f57c8af
ricksonre <ricksonre@gmail.com>
2020-01-18 13:48:54 -0800

    Update engine.js

--- Tatercoin/Base/engine.js
+++ Tatercoin/Base/engine.js
@@ -3,12 +3,57 @@ class Engine
     constructor()
     {
         this.time_constant = 0.0166667;
+        this.time = new Timer();
+        this.objects = [];
+        this.grid = new Cell[32][18];
     }
     
+    add_object()
+    {
+
+    }
+
     load()
+    {
+
+    }
 
     run()
+    {
+        this.time.restart();
 
+        while(true)
+        {
+            
+            if(this.time.get_time_millis() > this.time_constant)
+            {
+                //input
+
+                //logic
+                
+                //draw
+
+
+                this.time.restart();
+            }
+
+        }
+    }
+
+    input()
+    {
+
+    }
+
+    update()
+    {
+
+    }
+
+    draw()
+    {
+
+    }
 }
 
 class cell
@@ -16,6 +61,7 @@ class cell
 
 }
 
+//timer
 class Timer
 {
     constructor()
@@ -32,4 +78,21 @@ class Timer
     {
         this.curr_time = new Date().getTime();
     }
+}
+
+//a game object
+// the child needs to overwrite the execure function and implement its logic
+// 1 execute per tick
+class Object
+{
+    constructor()
+    {
+        this.id = -1;
+        this.name = "";
+        this.level = 0;
+    }
+
+    execute();
+
+
 }
