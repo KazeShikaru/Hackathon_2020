ba0b356b962eb2cd3076a510dcf71f63c3587e73
Rock-it-science <wilmc17@gmail.com>
2020-01-19 09:39:24 -0800

    Adding building switch

--- Tatercoin/Base/UI.js
+++ Tatercoin/Base/UI.js
@@ -14,10 +14,42 @@ $(document).ready(function()
         );
 
 
-        
+
     });
 
-    function currentMenu(pos)
+    function currentMenu(building)
     {
+      /*
+      1 = farm
+        - Plant button
+        - Harvest button
+        - Current state
+      2 = storage
+        - Things stored
+      3 = Empty land
+        - Nothing
+      */
+      /*
+      var btn = document.createElement("BUTTON");   // Create a <button> element
+      btn.innerHTML = "CLICK ME";                   // Insert text
+      document.body.appendChild(btn);               // Append <button> to <body>
+      */
+      switch(building){
 
+        case 1: //Farm
+          var plantbtn = document.createElement('BUTTON');
+          plantbtn.onclick = ObjectManager.arr[1].plant(10);
+          plantbtn.innerHTML = "Plant 10 seeds";
+          document.body.appendChild(plantbtn);
+
+          var harvestbtn = document.createElement('BUTTON');
+          plantbtn.onclick = ObjectManager.arr[1].harvest();
+          harvestbtn.innerHTML = "harvest";
+          document.body.appendChild(harbestbtn);
+
+          var currentState = document.createElement('p');
+          currentState.innerText = "Planted: " + ObjectManager.arr[1].planted + ", harvestable: " + ObjectManager.arr[1].harvestable;
+          document.body.appendChild(currentState);
+          break;
+      }
     }
