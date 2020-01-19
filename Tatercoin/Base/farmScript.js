211d21c6925b983aee2820ac55c57aa53fa7393b
Rock-it-science <wilmc17@gmail.com>
2020-01-18 17:46:12 -0800

    Moving functions from index to farmScript

--- Tatercoin/Base/farmScript.js
+++ Tatercoin/Base/farmScript.js
@@ -8,4 +8,34 @@ module.exports = {
 		}
 		return null;
 	}
+	
+	//Plant a harvest of potatoes
+	function plant(seedsToPlant){
+		if(!_farm.planted){//Make sure not planted already
+			if(seedsToPlant <= seeds){//Make sure player has enough seeds
+				console.log('Planting ' + seedsToPlant + ' seeds');
+				_farm.planted = true;
+				_farm.plantedSeeds = seedsToPlant;
+				seeds-= seedsToPlant;
+				console.log('User now has '+seeds+' seeds');
+			}
+			else{
+				console.log('Not enough seeds');
+			}
+		}
+		else{
+			console.log('Already planted');
+		}
+	}
+
+	function harvest(){
+		if(_farm.planted){//Make sure farm is planted
+			//TODO Get upgrade modifiers here when they're implemented
+			var harvestYield = _farm.plantedSeeds * math.random();
+			store("potatoes", harvestYield);
+		}
+		else{
+			console.log('Farm not planted');
+		}
+	}
 };
