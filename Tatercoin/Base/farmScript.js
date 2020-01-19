b42c952da295ae09c8d4218797f17176dceae899
Rock-it-science <wilmc17@gmail.com>
2020-01-18 16:35:52 -0800

    remove harvest function from farmScript and change return location

--- Tatercoin/Base/farmScript.js
+++ Tatercoin/Base/farmScript.js
@@ -2,17 +2,10 @@ module.exports = {
 	run: farm(element){
 		if(growAmount < 10 && element.planted = true){ // If planted, grow
 			element.growAmount += element.growPerTick;
-			return null;
 		}
 		else { // If fully grown, set harvestable
 			element.harvestable = true;
-			//Harvest
-			element.onclick = function() {
-				element.growAmount = 0;
-				element.planted = false;
-				return 2;
-			}
-			return null;
 		}
+		return null;
 	}
 };
