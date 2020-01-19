26776e87fa595d514c4a2e2141956ec38a857006
ricksonre <ricksonre@gmail.com>
2020-01-18 21:38:53 -0800

    Update EventHandler.js

--- Tatercoin/Base/EventHandler.js
+++ Tatercoin/Base/EventHandler.js
@@ -3,16 +3,19 @@ class EventHandler
     constructor()
     {
         this.canvas = document.getElementById('canvas');
+        this.cell_width = 69;
     }
 
     init()
     {
-        this.canvas.addEventListener('mousedown', this.getGridPos())
+        this.canvas.addEventListener('mousedown', e => {this.getGridPos(canvas, e)})
     }
 
-    getGridPos()
+    getGridPos(canvas, event)
     {
-
+        let rect = canvas.getBoundingClientRect(); 
+        x = event.clientX - rect.left;
+        y = event.clientY - rect.top;
     }
     
 }
