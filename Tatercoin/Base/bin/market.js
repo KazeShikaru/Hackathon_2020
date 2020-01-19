0937547568279c7e6cdb37f756b70e26ad7deba7
Carter <carter.joel@gmx.com>
2020-01-18 22:02:30 -0800

    market  and marketscript

---
+++ Tatercoin/Base/bin/market.js
@@ -0,0 +1,8 @@
+class market extends Object
+{
+	constructor()
+	{
+		super(4, "market");
+		add = 0;
+	}
+}
---
+++ Tatercoin/Base/marketScript.js
@@ -0,0 +1,8 @@
+function marketS()
+{
+	var element = ObjectManager.objRet[4];
+	if(element.add > 0)
+	{
+		ObjectManager.addCoins(add);
+	}
+}
