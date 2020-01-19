95962c0577a83f61b898d9fbd4852f8159905bc8
Carter <carter.joel@gmx.com>
2020-01-18 21:47:53 -0800

    Update processingScript.js

--- Tatercoin/Base/processingScript.js
+++ Tatercoin/Base/processingScript.js
@@ -1,14 +1,9 @@
 function ProcessingS(element)
 {
-	var element = ObjectManager.arr[2];
+	var element = ObjectManager.arr[3];
 	if(element.add > 0)
 	{
 		element.potatoes += add;
 		add = 0;
-	}
-
-	if(peeling)
-	{
-		potatoes
 	}
 }
