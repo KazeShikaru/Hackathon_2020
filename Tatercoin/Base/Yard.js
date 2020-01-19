d5b9a49b848b2644f6a2b3ecbaea0194dd377bc3
Carter <carter.joel@gmx.com>
2020-01-19 03:14:16 -0800

    updated render values

--- Tatercoin/Base/Yard.js
+++ Tatercoin/Base/Yard.js
@@ -12,7 +12,7 @@ class Yard
             	if(j<4 && i < 4)
             	{
             		this.grid[i][j] = new Array(4);
-               		this.grid[i][j] = [1,0,0,0];
+               		this.grid[i][j] = [1,1,0,0];
             	}
             	else if(j>4 && i < 4)
             	{
@@ -22,7 +22,7 @@ class Yard
             	else
             	{
             		this.grid[i][j] = new Array(4);
-                	this.grid[i][j] = [0,1,0,0];
+                	this.grid[i][j] = [0,0,0,0];
             	}
 				//[0]building type//[1]sprite stage//[2]growthstage//[3]Unique
 				//console.log(this.grid)
