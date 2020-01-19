231b5b67b0ce2362211f622d2d0c80f9ad89c86f
ricksonre <ricksonre@gmail.com>
2020-01-18 21:24:22 -0800

    Create EventHandler.js

---
+++ Tatercoin/Base/EventHandler.js
@@ -0,0 +1,19 @@
+class EventHandler
+{
+    constructor()
+    {
+        this.canvas = document.getElementById('canvas');
+    }
+
+    init()
+    {
+        this.canvas.addEventListener('mousedown', this.getGridPos())
+    }
+
+    getGridPos()
+    {
+
+    }
+    
+}
+
