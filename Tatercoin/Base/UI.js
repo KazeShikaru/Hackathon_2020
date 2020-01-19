642b6b0fc442393b59474ecf3a458aea07346474
ricksonre <ricksonre@gmail.com>
2020-01-19 05:23:59 -0800

    asd

---
+++ Tatercoin/Base/UI.js
@@ -0,0 +1,17 @@
+
+$('#buy10Seeds').click()
+(
+    ObjectManager.buySeeds(10)
+);
+
+$('#buySeeds').click()
+(
+    ObjectManager.buySeeds(1)
+);
+
+$('#seeds').html
+
+function currentMenu(pos)
+{
+
+}
