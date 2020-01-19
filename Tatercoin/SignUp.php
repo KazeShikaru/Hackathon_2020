4e2587a46e599f7465f47958be65182cdd4532cb
Kaze <renyuan7777@gmail.com>
2020-01-19 02:49:50 -0800

    Signin proecess start

---
+++ Tatercoin/SignUp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+<form action="/index.php">
+  First name: <input type="text" name="fname"><br>
+  Last name: <input type="text" name="lname"><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+
+</html>
